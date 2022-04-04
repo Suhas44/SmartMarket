7dd5b988d14bc2d20833f5c86a64ac828369037c
Suhas44 <suhas8@outlook.com>
2022-04-03 21:12:42 -0500

    More Changes

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use('/', express.static(path.join(__dirname, 'static')));
+app.use(bodyParser.json());
+
+app.post('/api/register', async(req, res) => {
+    console.log(request, body)
+});
 
 app.listen(9999, () => {
         console.log('Server up at port 9999');
