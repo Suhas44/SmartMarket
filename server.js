461ad61dcc86932ccefd2de9b967a48b91896dc5
Suhas44 <suhas8@outlook.com>
2022-04-13 12:12:22 -0500

    Actually Reading From Mongo

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const { read } = require('fs');
+const res = require('express/lib/response');
 
 const app = express();
 app.use('/', express.static(path.join(__dirname, 'static')));
@@ -27,11 +29,29 @@ async function writeToAtlas(obj) {
     var url = "mongodb+srv://MongoTest:MongoTester1@cluster0.5oqhq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
+      console.log("Im inside now")
       var dbo = db.db("Users");
-      dbo.collection("List").insertOne(obj, function(err, res) {
+      var collection = dbo.collection("List");
+      collection.insertOne(obj, function(err, res) {
         if (err) throw err;
         console.log("1 document inserted");
         db.close();
       });
     });
 }
+
+async function findOneListingByName(collection, nameOfListing) {
+    const result = collection.find({"username": nameOfListing}); 
+    return result;
+}
+
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://MongoTest:MongoTester1@cluster0.5oqhq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+MongoClient.connect(url, function(err, db) {
+  var dbo = db.db("Users");
+  var coll = dbo.collection("List");
+  findOneListingByName(coll, "Suhas").then(result => {
+    console.log(result);
+  });
+});
