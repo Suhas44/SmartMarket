cf47edb5082478804c77545997b319c89c71a278
Suhas44 <suhas8@outlook.com>
2022-04-15 15:13:25 -0500

    More Querying

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const { read } = require('fs');
 const app = express();
+
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb+srv://MongoTest:MongoTester1@cluster0.5oqhq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
 app.use('/', express.static(path.join(__dirname, 'static')));
 app.use(bodyParser.json());
 
+// Sending registering users to MongoDB
 app.post('/index', async(req, res) => {
     console.log(req.body)
     writeToAtlas(req.body);
     res.json({ status: 'ok' })
 });
 
+/*
+*** This is an example of posting data, I will keep this here for reference. The corresponding FETCH command is in list.html***
 app.post('/theusers', async(req, res) => {
-  console.log(req)
   res.json({ status: 'ok' })
+  console.log("the thing: " + req.body.username);
+  readAtlas("username", req.body.username);
 });
+*/
 
 app.listen(9999, () => {
-        console.log('Server up at port 9999');
+  console.log('Server up at port 9999');
 });
 
 async function writeToAtlas(obj) {
     console.log("CALLED!");
-    var MongoClient = require('mongodb').MongoClient;
-    var url = "mongodb+srv://MongoTest:MongoTester1@cluster0.5oqhq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
     MongoClient.connect(url, function(err, db) {
       if (err) throw err;
       console.log("Im inside now")
@@ -38,12 +45,25 @@ async function writeToAtlas(obj) {
     });
 }
 
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb+srv://MongoTest:MongoTester1@cluster0.5oqhq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-MongoClient.connect(url, async function(err, db) {
-  var dbo = db.db("Users");
-  c = dbo.collection("List");
-  r = await c.find({"username" : "Suhas"}).toArray();
-  console.log(r[0]);
-});
+function readAtlas(username, user) {
+  MongoClient.connect(url, async function(err, db) {
+    var dbo = db.db("Users");
+    c = dbo.collection("List");
+    r = await c.find({username:user}).toArray();
+  });
+}
 
+async function readAtlasAll() {
+  const db = await MongoClient.connect(url);
+  const dbo = db.db("Users");
+  const c = dbo.collection("List");
+  const r = await c.find({}).toArray();
+  console.log(r);
+  return r;
+}
+
+readAtlasAll().then(r => {
+ console.log(r);
+}).catch(err => {
+  console.log(err);
+});
