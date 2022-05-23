004890c2955d174dc6ecb32e96713bf92788d423
Suhas44 <suhas8@outlook.com>
2022-05-23 18:20:28 -0500

    Sign Up Restrictions

--- public/src/index.js
+++ public/src/index.js
@@ -7,21 +7,28 @@ async function registerUser(event) {
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    document.getElementById("username").value = "";
-    document.getElementById("password").value = "";
+    function credentialRestrictions(username, password) {
+        return ((username.match("^[A-Za-z0-9]+$")) && (password.match("^[A-Za-z0-9]+$") && (username.length >= 4) && (password.length >= 4)));
+    }
 
-    const result = await fetch("/register", {
-        method: "POST",
-        headers: {
-        "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-        username: username,
-        password: password,
-        }),
-    }).then((response) => response.json()).then((data) => { 
-        alert(data.message);    
-    });
+    if (credentialRestrictions(username, password)) {
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
+        const result = await fetch("/register", {
+            method: "POST",
+            headers: {
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+            username: username,
+            password: password,
+            }),
+        }).then((response) => response.json()).then((data) => { 
+            alert(data.message);    
+        });
+    } else {
+        alert("Username and password must be at least 4 characters long and contain only letters and numbers");
+    }
 }
 
 //authentication
